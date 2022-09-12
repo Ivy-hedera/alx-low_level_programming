30f1d85a5876cd6dfbc38b7dd27125db27cc5d57
Ivy-hedera <ivy413b@gmail.com>
2022-09-12 10:07:04 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: sizes of bytes
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("Size of a char: %d bytes(s)\n", sizeof(c));
+	printf("Size of an int: %d bytes(s)\n", sizeof(i));
+	printf("Size of a long int: %d bytes(s)\n", sizeof(li));
+	printf("Size of a long long int: %d bytes(s)\n", sizeof(lli));
+	printf("Size of a float: %d bytes(s)\n", sizeof(f));
+	return (0);
+}
